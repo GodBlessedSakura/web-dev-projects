c0ac9fd11a7eb4bb2142e3ceaf64ffbe896f633d
Zhang Lin <1306562902@qq.com>
2023-02-23 16:43:00 +0800

    improve: 个性化调整页面内容，优化部分UI

--- personal-portfolio/server.js
+++ personal-portfolio/server.js
@@ -1,52 +1,50 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+const express = require('express')
+const router = express.Router()
+const cors = require('cors')
+const nodemailer = require('nodemailer')
 
 // server used to send send emails
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(5000, () => console.log('Server Running'))
 
 const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
+  service: 'qq',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: '1306562902@qq.com',
+    pass: 'necgdfvfhtdtjeja',
   },
-});
+})
 
 contactEmail.verify((error) => {
   if (error) {
-    console.log(error);
+    console.log(error)
   } else {
-    console.log("Ready to Send");
+    console.log('Ready to Send')
   }
-});
+})
 
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
+router.post('/contact', (req, res) => {
+  const name = req.body.firstName + req.body.lastName
+  const email = req.body.email
+  const message = req.body.message
+  const phone = req.body.phone
   const mail = {
-    from: name,
-    to: "********@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
+    from: '1306562902@qq.com',
+    to: '1306562902@qq.com',
+    subject: 'Contact Form Submission - Portfolio',
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
            <p>Phone: ${phone}</p>
            <p>Message: ${message}</p>`,
-  };
+  }
   contactEmail.sendMail(mail, (error) => {
     if (error) {
-      res.json(error);
+      res.json(error)
     } else {
-      res.json({ code: 200, status: "Message Sent" });
+      res.json({ code: 200, status: 'Message Sent' })
     }
-  });
-});
+  })
+})
