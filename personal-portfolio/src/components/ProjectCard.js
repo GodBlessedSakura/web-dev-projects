08de66258fe2ba48a4e1791564f57617fe68049d
Zhang Lin <1306562902@qq.com>
2023-02-24 15:48:05 +0800

    improve: 修改项目

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -1,13 +1,15 @@
-import { Col } from "react-bootstrap";
+import { Col } from 'react-bootstrap'
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, pageUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <span>{description}</span>
+          <a href={pageUrl} style={{ color: '#fff', textDecoration: 'none' }} target="_blank" rel="noreferrer">
+            <span>{description}</span>
+          </a>
         </div>
       </div>
     </Col>
--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -1,46 +1,56 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import { Container, Row, Col, Tab, Nav } from 'react-bootstrap'
+import { ProjectCard } from './ProjectCard'
+import pinyouImg1 from '../assets/img/pinyou-img1.JPG'
+import pinyouImg2 from '../assets/img/pinyou-img2.JPG'
+import pinyouImg3 from '../assets/img/pinyou-img3.JPG'
+import mapdiaryImg1 from '../assets/img/mapdiary-img1.JPG'
+import mapdiaryImg2 from '../assets/img/mapdiary-img2.JPG'
+import mapdiaryImg3 from '../assets/img/mapdiary-img3.JPG'
+import colorSharp2 from '../assets/img/color-sharp2.png'
+import 'animate.css'
+import TrackVisibility from 'react-on-screen'
 
 export const Projects = () => {
-
-  const projects = [
+  const pinyou = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: 'Pinyou Shopping',
+      description: 'Go to page',
+      pageUrl: '/shopping',
+      imgUrl: pinyouImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: 'Pinyou Shopping',
+      description: 'Go to page',
+      pageUrl: '/shopping',
+      imgUrl: pinyouImg2,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: 'Pinyou Shopping',
+      description: 'Go to page',
+      pageUrl: '/shopping',
+      imgUrl: pinyouImg3,
+    },
+  ]
+  const mapdiary = [
+    {
+      title: 'Map Diary',
+      description: 'Go to page',
+      pageUrl: '/map',
+      imgUrl: mapdiaryImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: 'Map Diary',
+      description: 'Go to page',
+      pageUrl: '/map',
+      imgUrl: mapdiaryImg2,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: 'Map Diary',
+      description: 'Go to page',
+      pageUrl: '/map',
+      imgUrl: mapdiaryImg3,
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-  ];
+  ]
 
   return (
     <section className="project" id="project">
@@ -48,46 +58,65 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div className={isVisible ? 'animate__animated animate__fadeIn' : ''}>
+                  <h2>Projects</h2>
+                  <p>
+                    During my self-study in website development, I completed several projects including e-commerce website and map-diary website. These projects were aimed at improving my coding skills and website development experience, while also allowing me to gain an understanding of both
+                    front-end and back-end development processes.
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Pinyou Shopping</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Map Diary</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Silent Chat</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? 'animate__animated animate__slideInUp' : ''}>
+                      <Tab.Pane eventKey="first">
+                        <p>
+                          Pinyou Shopping is a static e-commerce Web App with home page, login page and list page. In this project, I practised the entensive usage of HTML5 and CSS3 for page layout and detailed styles. I used original JS API to add a few animations such as countdown boxes and
+                          carousel figures.
+                        </p>
+
+                        <Row>
+                          {pinyou.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>Map Diary is a light-weight web application where you can keep diaries and record places where exciting moments happens. Click on the link below, create an account and have a try!</p>
+                        <Row>
+                          {mapdiary.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>
+                          With ChatGPT becoming increasingly popular, I am also interested in such a phenomenal AI language model, and that's why I developed this mini small tool. Silent Chat is an auto-reply tool for Wechat which utilizes OCR to detect and parsing incoming messages on your PC,
+                          forwards it to ChatGPT and generates responses automatically. From extensive tests, the OCR recognition accuracy is around 94% for pure text, which sounds a good performance. However, it seems quite difficult to optimize Silent Chat simply with OCR detection. Here are some
+                          reasons:
+                          <hr style={{ visibility: 'hidden' }} />
+                          <ul style={{ listStyle: 'none', textAlign: 'left' }}>
+                            <li>1. The user have to keep Wechat client in the front window (no user experience at all!)</li>
+                            <li>2. Hard to generate a stable and unique id for each conversation to maintain a record of chat contexts and let ChatGPT "remember" each conversation.</li>
+                            <li>3. Unbale to understand emojis and pictures.</li>
+                            <li>4. Occational wrong parsing of characters.</li>
+                          </ul>
+                          Unfortunately, Wechat does not offer any API to directly log in to account and fetch conversation details. Otherwise, this chatbot tool would be more useful and practical. Looking forward to someday when Wechat can provide such APIs.
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
